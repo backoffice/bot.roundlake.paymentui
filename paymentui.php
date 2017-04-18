08d54134f47c93b19ab8b64f96421362607e6816
Alice Frumin <alice@aghstrategies.com>
2017-04-18 10:30:29 -0400

    cleaning up

--- paymentui.php
+++ paymentui.php
@@ -5,19 +5,24 @@ require_once 'paymentui.civix.php';
 
 /**
  * Implementation of hook_civicrm_tokens
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_tokens
  */
 function paymentui_civicrm_tokens(&$tokens) {
   $tokens['partialPayment'] = array(
     'partialPayment.table' => 'Table of Partial Payment Information',
   );
 }
+
 /**
- * Implementation of hook_civicrm_tokensValues
+ * Implementation of hook_civicrm_tokenValues
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_tokenValues
  */
-function paymentui_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+function paymentui_civicrm_tokenValues(&$values, $cids, $job = NULL, $tokens = array(), $context = NULL) {
   if (!empty($tokens['partialPayment'])) {
     foreach ($cids as $contactID) {
-      $table =  '
+      $table = '
          <table class="partialPayment">
            <thead><tr>
              <th>Event Name</th>
@@ -28,29 +33,33 @@ function paymentui_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = a
            </tr></thead>
            <tbody>';
       $participantInfo = CRM_Paymentui_BAO_Paymentui::getParticipantInfo($contactID);
-        foreach ($participantInfo as $row) {
-          $table .= "
-           <tr class=" . $row['rowClass'] . ">
-             <td>" . $row['event_name'] . "</td>
-             <td>" . $row['contact_name'] . "</td>
-             <td>" . $row['total_amount'] . "</td>
-             <td>" . $row['paid'] . "</td>
-             <td>" . $row['balance'] . "</td>
-           </tr>
-         ";
-       }
-       $table .= "</tbody></table>";
-       $partialPaymentTokens = array(
-         'partialPayment.table' => $table,
-       );
-       $values[$contactID] = empty($values[$contactID) ? $partialPaymentTokens : $values[$contactID] + $partialPaymentTokens;
-     }
-   }
- }
+      foreach ($participantInfo as $row) {
+        $table .= "
+         <tr class=" . $row['rowClass'] . ">
+           <td>" . $row['event_name'] . "</td>
+           <td>" . $row['contact_name'] . "</td>
+           <td>" . $row['total_amount'] . "</td>
+           <td>" . $row['paid'] . "</td>
+           <td>" . $row['balance'] . "</td>
+         </tr>
+       ";
+      }
+      $table .= "</tbody></table>";
+      $partialPaymentTokens = array(
+        'partialPayment.table' => $table,
+      );
+      $values[$contactID] = empty($values[$contactID]) ? $partialPaymentTokens : $values[$contactID] + $partialPaymentTokens;
+    }
+  }
+}
 
-//on back end we will send receipt when this field is populated for custom field
+/**
+ * Implementation of hook_civicrm_custom
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_custom
+ */
 function paymentui_civicrm_custom($op, $groupID, $entityID, &$params) {
-  // when this field is populated send receipt email with token
+  // on back end we will send receipt with token when this field is populated for custom field
   // TODO create message template with ID
   // TODO create custom field
   $mailParams = array(

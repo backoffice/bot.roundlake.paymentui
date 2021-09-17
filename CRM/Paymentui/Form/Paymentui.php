b3be220448c05395ecb7140fbcd061cf626f34e7
BOTKevinS <kstanek@backofficethinking.com>
2021-09-17 11:32:56 -0400

    Update Paymentui.php
    replacing deprecated method doDirectPayment with doPayment.  Noting that there have been many changes to this file in the past which have either been commited to another branch and never merged or never committed at all....

--- CRM/Paymentui/Form/Paymentui.php
+++ CRM/Paymentui/Form/Paymentui.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'CRM/Core/Form.php';
-
+require_once 'CRM/Core/Error.php';
 /**
  * Form controller class
  */
@@ -23,14 +23,34 @@ public $_params;
 	$this->assign('bltID', $this->_bltID);
 	$this->_fields = array();
 
-	CRM_Core_Payment_Form::setCreditCardFields($this);
+	//Gets the live default payment processor, if not found displays an error
+    try {
+	  $ppResult = civicrm_api3('PaymentProcessor', 'get', array(
+	    'sequential' => 1,
+	    'return' => "id,name,payment_processor_type_id",
+	    'is_default' => 1,
+	    'is_active' => 1,
+	    'is_test' => 0,
+    	));
+    }
+	catch (CiviCRM_API3_Exception $e) {
+      $error = $e->getMessage();
+	  CRM_Core_Error::fatal("Payment Processor is not set for processing the payment. Please contact the system administrator");
+    }
+	
+	if ( !$ppResult['error'] ) {
+		$this->payment_processor_id = $ppResult['id'];
+	} else {
+		//CRM_Core_Error::fatal("Payment Processor is not set for processing the payment. Please contact the system administrator");
+		$message = ts ('Payment Processor is not set for processing the payment. Please contact the system administrator.');
+		CRM_Utils_System::setUFMessage($message);		
+		return;
+	}
+	$this->_paymentProcessor = CRM_Financial_BAO_PaymentProcessor::getPayment( $this->payment_processor_id, 'live' );
+
+	CRM_Core_Payment_Form::setPaymentFieldsByProcessor($this, $this->_paymentProcessor);
 	$this->assign_by_ref('paymentProcessor', $paymentProcessor);
 	$this->assign('hidePayPalExpress', TRUE);
-	//Set Payment processor to CC
-	
-	$this->payment_processor_id = CRM_Core_DAO::getFieldValue( 'CRM_Financial_DAO_PaymentProcessor', 'Credit Card', 'id', 'name');
-
-	$this->_paymentProcessor = CRM_Financial_BAO_PaymentProcessor::getPayment( $this->payment_processor_id, 'live' );
   }
 	
   /**
@@ -60,11 +80,22 @@ public $_params;
 	//Get event names for which logged in user and the related contacts are registered
 	$this->_participantInfo = CRM_Paymentui_BAO_Paymentui::getParticipantInfo( $this->_contactId );
 	$this->assign('participantInfo', $this->_participantInfo );
-	foreach ( $this->_participantInfo as $pid => $pInfo ) {
-		$element =& $this->add( 'text', "payment[$pid]", null, array( 'onblur' => 'calculateTotal();' ),	false);
+	$totalAmount = 0;
+	if ( $this->_participantInfo ) {
+		foreach ( $this->_participantInfo as $pid => $pInfo ) {
+			$totalAmount += $pInfo['total_amount'];
+			$element =& $this->add( 'text', "payment[$pid]", null, array( 'onblur' => 'calculateTotal();' ),	false);
+	
+	
+		}
 	}
-	CRM_Contribute_Form_ContributionBase::assignToTemplate();
-	CRM_Core_Payment_Form::buildCreditCard($this);	
+
+	$element =& $this->add( 'text', "totalAmount", $totalAmount,	false);
+	$this->assign('totalAmount', $totalAmount);
+
+    //BOT commented out due to issues with CiviCRM 5.x and PHP 7
+	//CRM_Contribute_Form_ContributionBase::assignToTemplate();
+	CRM_Core_Payment_Form::buildPaymentForm($this, $this->_paymentProcessor, FALSE, FALSE);	
     
     $this->addButtons(array(
       array(
@@ -163,21 +194,46 @@ public $_params;
 	$this->_params['invoiceID']      = md5(uniqid(rand(), TRUE));
  
 	$paymentParams = $this->_params;
-	CRM_Core_Payment_Form::mapParams($this->_bltID, $this->_params, $paymentParams, TRUE);
-	$payment = CRM_Core_Payment::singleton($this->_mode, $this->_paymentProcessor, $this);
+    
+	//singleton function is not there anymore in specified file in version 5
+	//$payment = CRM_Core_Payment::singleton($this->_mode, $this->_paymentProcessor, $this);
+	//added this below line to get payment processor info
+ 	$payment = Civi\Payment\System::singleton()->getByProcessor($this->_paymentProcessor);
+	$result  = $payment->doPayment($paymentParams);
+        //CRM_Core_Error::debug_var("resultCodeFromExtension", "<pre>" . print_r($result,TRUE) . "</pre>");   
+        //CRM_Core_Error::debug_var("madness", "yep");
+ 
+     // if( isset($result['trxn_id']) && !empty($result['trxn_id']) ) {
+        if ( is_array($result) && isset($result['trxn_id']) && !empty($result['trxn_id'])  ) {
 
-	$result  = $payment->doDirectPayment($paymentParams);
-	$CCFinancialTrxn = CRM_Paymentui_BAO_Paymentui::createFinancialTrxn( $paymentParams );
+          ///CRM_Core_Error::debug_var("shouldNotSeeThis", $statusMsg);
 
-	$partialPaymentInfo = $this->_participantInfo;
+          //Create financial transaction and entity financial transaction records
+         $CCFinancialTrxn = CRM_Paymentui_BAO_Paymentui::createFinancialTrxn( $paymentParams, $this->payment_processor_id );
+  
+	  $partialPaymentInfo = $this->_participantInfo;
+    
     //Process all the partial payments and update the records
-	payment_civicrm_process_partial_payments( $paymentParams, $this->_participantInfo );
-	parent::postProcess();
+    //Function defined in bot.partial.payment extension - payment.php
+ 	  payment_civicrm_process_partial_payments( $paymentParams, $this->_participantInfo );
+	  parent::postProcess();
   
-	//Define status message
-	$statusMsg = ts('The payment(s) have been processed successfully.');
-	CRM_Core_Session::setStatus($statusMsg, ts('Saved'), 'success');
+	 //Define status message
+	 $statusMsg = ts('The payment(s) have been processed successfully.');
+    	CRM_Core_Session::setStatus($statusMsg, ts('Saved'), 'success');
+    } else {
+  	$statusMsg = ts('Sorry, an error occurred while processing the transaction. Please try again.');
+        //CRM_Core_Error::debug_var("didwegethere", "thishastoShow");
+    	CRM_Core_Session::setStatus(ts('Sorry, an error occurred while processing the transaction. Please try again.'), ts('Error'), 'error');
+    }
   
+
+        //debug code for storing payment results to error log, some payments failing at payment processor but no error message and civi thinks payment succeeded
+        //Debug var of payment params
+        //unset cvv and credit card number
+        //unset($result['cvv2']);
+        //unset($result['credit_card_number']);
+
 	//Redirect to the same URL
 	$url     = CRM_Utils_System::url('civicrm/roundlake/add/payment', "reset=1" ); 
 	$session = CRM_Core_Session::singleton();
